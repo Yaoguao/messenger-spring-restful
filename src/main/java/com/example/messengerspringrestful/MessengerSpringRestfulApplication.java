5fc9472e59e71f91705588e3251adc7855b1703a
Yaoguao <126403423+Yaoguao@users.noreply.github.com>
2024-05-18 13:35:43 +0400

    version 0.1

---
+++ src/main/java/com/example/messengerspringrestful/MessengerSpringRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.messengerspringrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerSpringRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerSpringRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/messengerspringrestful/MessengerSpringRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messengerspringrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerSpringRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
